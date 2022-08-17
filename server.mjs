c917e87a097dc87a6c18d6b300453abab966064a
mariadhantia <mariadhantia@gmail.com>
2022-08-18 04:28:08 +0500

    updated

--- server.mjs
+++ server.mjs
@@ -1,87 +1,117 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express"
+import cors from "cors"
 import { nanoid } from 'nanoid'
 
-const app = express()
-app.use(express.json());///express ka code h//
-app.use((cors));
 
-// console.log( nanoid () );////terminal pe id dega port start ktne pe///
+const app = express();
+app.use(express.json());
+app.use(cors());
+
 
 const port = process.env.PORT || 3000;
 
-let userBase= [];  //data base ka wo hissa jha sare users save hote h unko userBase khte h//
- 
-app.post("/signup",(req ,res)=>{
 
-   let body= req.body;
+let userBase = []; // TODO: replace this with mongoDB
 
-    if(!body.firstName || !body.lastName || !body.name || !body.email ||!body.passward)
-    {
-/// this(400) status shows client error///
+app.post("/signup", (req, res) => {
+
+    let body = req.body;
+
+    if (!body.firstName
+        || !body.lastName
+        || !body.email
+        || !body.password
+    ) {
         res.status(400).send(
-            `required fields missing,request example:
-        {
-            "firstName":"Jhon",
-            "lastName":"Doe",
-            "email":"abc@abc.com",
-            "passward":"12345"
-        }`
+            `required fields missing, request example: 
+                {
+                    "firstName": "John",
+                    "lastName": "Doe",
+                    "email": "abc@abc.com",
+                    "password": "12345"
+                }`
         );
         return;
     }
-///////////////////////validation end nw making new user///
-//////for making id we use(NanoID)////
-    let newUser = {
-         userId:   nanoid(),
-        firstName: body.firstName,
-        lastName:  body.lastName,
-         email:     body.email,
-        passward:  body.passward,
 
+    let isFound = false;
+
+    for (let i = 0; i < userBase.length; i++) {
+        if (userBase[i].email === body.email.toLowerCase()) {
+            isFound = true;
+            break;
+        }
+    }
+    if (isFound) { // this email already exist
+        res.status(400).send({
+            message: `email ${body.email} already exist.`
+        });
+        return;
+    }
+
+
+    let newUser = {
+        userId: nanoid(),
+        firstName: body.firstName,
+        lastName: body.lastName,
+        email: body.email.toLowerCase(),
+        password: body.password
     }
 
     userBase.push(newUser);
 
-    res.status(201).send("user is created");
+    res.status(201).send({ message: "user is created" });
+});
 
- });
- ////////login server///
-   let  isfound = false;  //https://stackoverflow.com/a/17402180(what is flag variable)
+app.post("/login", (req, res) => {
 
-     for (let i = 0; i < userBase.length; i++){
+    let body = req.body;
 
-        if(userBase[i].email === body.email){///corect paasward//
-
-           isfound =true;
-
-           if(userBase[i].passward === body.passward){
-
-                res.statu(200).send({
-                firstName:userBase[i].firstName,
-                lastName:userBase[i].lastName,
-                email:userBase[i].email,
-                massage:"login successful",
-                token:"some unique token"
-
-            })
-            return;
-
-        }else{//passward inorrect//
-            res.status(401).send({
-              massage:"incorrect passward"  
-            })
-            return;
-        }
-
-     }
+    if (!body.email || !body.password) {
+        res.status(400).send(
+            `required fields missing, request example: 
+                {
+                    "email": "abc@abc.com",
+                    "password": "12345"
+                }`
+        );
+        return;
     }
-    if (!isfound){
+
+    let isFound = false; // https://stackoverflow.com/a/17402180/4378475
+
+    for (let i = 0; i < userBase.length; i++) {
+        if (userBase[i].email === body.email) {
+
+            isFound = true;
+            if (userBase[i].password === body.password) { // correct password
+
+                res.status(200).send({
+                    firstName: userBase[i].firstName,
+                    lastName: userBase[i].lastName,
+                    email: userBase[i].email,
+                    message: "login successful",
+                    token: "some unique token"
+                })
+                return;
+
+            } else { // password incorrect
+
+                res.status(401).send({
+                    message: "incorrect password"
+                })
+                return;
+            }
+        }
+    }
+
+    if (!isFound) {
         res.status(404).send({
-            massage:"user not found",
+            message: "user not found"
         })
         return;
     }
+})
 
 
 
@@ -89,5 +119,171 @@ app.post("/signup",(req ,res)=>{
 
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
+
+// import express from "express"
+// import cors from "cors"
+// import { nanoid } from 'nanoid'
+
+// const app = express();
+// app.use(express.json());
+// app.use(cors());
+
+// // // console.log( nanoid () );////terminal pe id dega port start ktne pe///
+
+// const port = process.env.PORT || 3000;
+
+// //data base ka wo hissa jha sare users save hote h unko (userBase) khte h//
+// let userBase = []; // TODO: replace this with mongoDB
+
+//    app.post("/signup", (req, res) => {
+
+//     let body = req.body;
+
+//      //     if(!body.firstName || !body.lastName || !body.name || !body.email ||!body.passward)
+//      //     {
+//      // /// this(400) status shows client error///
+
+//     if (!body.firstName
+//         || !body.lastName
+//         || !body.email
+//         || !body.password
+//     ) {
+//         res.status(400).send(
+//             `required fields missing, request example: 
+//                 {
+//                     "firstName": "John",
+//                     "lastName": "Doe",
+//                     "email": "abc@abc.com",
+//                     "password": "12345"
+//                 }`
+//         );
+//         return;
+//     }
+
+//     let isFound = false;
+
+//     for (let i = 0; i < userBase.length; i++) {
+//         if (userBase[i].email === body.email.toLowerCase()) {
+//             isFound = true;
+//             break;
+//         }
+//     }
+//     if (isFound) { // this email already exist
+//         res.status(400).send({
+//             message: `email ${body.email} already exist.`
+//         });
+//         return;
+//     }
+
+
+//     // ///////////////////////validation end nw making new user///
+//     // //////for making id we use(NanoID)////
+//     let newUser = {
+//         userId: nanoid(),
+//         firstName: body.firstName,
+//         lastName: body.lastName,
+//         email: body.email.toLowerCase(),
+//         password: body.password
+//     }
+
+//     userBase.push(newUser);
+
+//     res.status(201).send({ message: "user is created" });
+//   });
+//     ///////login server////
+// app.post("/login", (req, res) => {
+
+//     let body = req.body;
+
+//     if (!body.email || !body.password) {
+//         res.status(400).send(
+//             `required fields missing, request example: 
+//                 {
+//                     "email": "abc@abc.com",
+//                     "password": "12345"
+//                 }`
+//         );
+//         return;
+//     }
+
+//   let isFound = false; // https://stackoverflow.com/a/17402180/4378475
+
+//     for (let i = 0; i < userBase.length; i++) {
+//     if (userBase[i].email === body.email) {
+
+//         isFound = true;
+//         if (userBase[i].password === body.password) { // correct password
+
+//             res.status(200).send({
+//                 firstName: userBase[i].firstName,
+//                 lastName: userBase[i].lastName,
+//                 email: userBase[i].email,
+//                 message: "login successful",
+//                 token: "some unique token"
+//             })
+//             return;
+
+//         } else { // password incorrect
+
+//             res.status(401).send({
+//                 message: "incorrect password"
+//             })
+//             return;
+//         }
+//     }
+//    }
+
+//     if (!isFound) {
+//     res.status(404).send({
+//         message: "user not found"
+//     })
+//     return;
+//   }
+//    })
+
+//     app.listen(port, () => {
+//    console.log(`Example app listening on port ${port}`)
+//     })
+//     ////////////////////////
+//     ////////sir code////
+//  ////////login server///
+// //    let  isfound = false;  //https://stackoverflow.com/a/17402180(what is flag variable)
+
+// //      for (let i = 0; i < userBase.length; i++){
+
+// //         if(userBase[i].email === body.email){///corect paasward//
+
+// //            isfound =true;
+
+// //            if(userBase[i].passward === body.passward){
+
+// //                 res.statu(200).send({
+// //                 firstName:userBase[i].firstName,
+// //                 lastName:userBase[i].lastName,
+// //                 email:userBase[i].email,
+// //                 massage:"login successful",
+// //                 token:"some unique token"
+
+// //             }
+// //                 )
+// //             return;
+
+// //         } else { // password incorrect
+
+// //             res.status(401).send({
+// //                 message: "incorrect password"
+// //             })
+// //             return;
+// //         }
+// //     }
+// // }
+
+// // if (!isFound) {
+// //     res.status(404).send({
+// //         message: "user not found",
+        
+// //     })
+// //     return;
+// // }
